cc5dcee1bd07d363e80df1aba7358c76f01d5213
Thomas George <private@thomas-george.com>
2023-01-12 14:28:09 +0100

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Thomas George on 12/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
